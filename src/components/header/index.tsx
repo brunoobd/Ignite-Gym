6cc2ef937e3f511c70cd1f3536a43d590932cc42
Bruno Duarte <bruno.duarte314@gmail.com>
2024-06-25 15:57:06 -0300

    header component added

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "./style";
+import Logo from "@assets/logo.svg";
+
+export const Header = () => (
+  <Container>
+    <Logo />
+    <Title>Treine sua mente e o seu corpo</Title>
+  </Container>
+);
