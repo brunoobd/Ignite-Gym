c5be1439bde5c97bf76c422904e908b0f707e27a
Bruno Duarte <bruno.duarte314@gmail.com>
2024-06-24 21:21:55 -0300

    theme added

---
+++ src/theme/index.ts
@@ -0,0 +1,29 @@
+export default {
+  COLORS: {
+    WHITE: "#FFFFFF",
+
+    GREEN_DARK: "#00875F",
+    GREEN_MID: "#00B37E",
+
+    GRAY_700: "#121214",
+    GRAY_600: "#202024",
+    GRAY_500: "#29292E",
+    GRAY_400: "#323238",
+    GRAY_300: "#7C7C8A",
+    GRAY_200: "#C4C4CC",
+    GRAY_100: "#E1E1E6",
+
+    RED: "#F75A68",
+  },
+  FONT_FAMILY: {
+    BOLD: "Roboto_700Bold",
+    REGULAR: "Roboto_400Regular",
+  },
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 20,
+  },
+};
