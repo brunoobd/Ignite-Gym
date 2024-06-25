7f401ce24e5eb1b2aeabc9d80dce216488ff68ad
Bruno Duarte <bruno.duarte314@gmail.com>
2024-06-24 21:27:17 -0300

    ThemeProvider added to App.tsx

--- App.tsx
+++ App.tsx
@@ -3,20 +3,22 @@ import {
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
-import { StatusBar, Text, View } from "react-native";
+import { ActivityIndicator, StatusBar, Text, View } from "react-native";
+import theme from "src/theme";
+import { ThemeProvider } from "styled-components/native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <ThemeProvider theme={theme}>
       <StatusBar
         barStyle={"light-content"}
         backgroundColor={"transparent"}
         translucent
       />
 
-      {fontsLoaded ? <Text>Hello World!</Text> : <></>}
-    </View>
+      {fontsLoaded ? <Text>Hello World!</Text> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
