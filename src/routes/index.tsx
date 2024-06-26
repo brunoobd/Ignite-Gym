9c21ba17c61388480c8aebbda6e34e29d67ff976
Bruno Duarte <bruno.duarte314@gmail.com>
2024-06-26 11:35:36 -0300

    background color added to Routes to prevent white background bug

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,19 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
+import { useTheme } from "styled-components/native";
+import { View } from "react-native";
 
-export const Routes = () => (
-  <NavigationContainer>
-    <AuthRoutes />
-  </NavigationContainer>
-);
+export const Routes = () => {
+  const { GRAY_700 } = useTheme().COLORS;
+  const theme = DefaultTheme;
+
+  theme.colors.background = GRAY_700;
+
+  return (
+    <View style={{ flex: 1, backgroundColor: GRAY_700 }}>
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </View>
+  );
+};
