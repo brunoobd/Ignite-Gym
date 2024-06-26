7dcb15304159c1909cd5306169a07ea28083b3d3
Bruno Duarte <bruno.duarte314@gmail.com>
2024-06-26 00:56:06 -0300

    SafeAreaProvider added to App.tsx

--- App.tsx
+++ App.tsx
@@ -3,7 +3,9 @@ import {
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
+import { SignUp } from "@screens/signup";
 import { ActivityIndicator, StatusBar, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import theme from "src/theme";
 import { ThemeProvider } from "styled-components/native";
 
@@ -11,14 +13,16 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle={"light-content"}
-        backgroundColor={"transparent"}
-        translucent
-      />
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle={"light-content"}
+          backgroundColor={"transparent"}
+          translucent
+        />
 
-      {fontsLoaded ? <Text>Hello World!</Text> : <ActivityIndicator />}
-    </ThemeProvider>
+        {fontsLoaded ? <SignUp /> : <ActivityIndicator />}
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
