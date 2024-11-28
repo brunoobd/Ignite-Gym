b16e546cab21de313c29c053dcbb09156ddc5484
Bruno Duarte <bruno.duarte314@gmail.com>
2024-11-27 22:44:31 -0300

    Remove styled-components from project

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import theme from "src/theme";
 import {
   Roboto_400Regular,
   Roboto_700Bold,
@@ -7,22 +6,19 @@ import {
 import { Routes } from "@routes/index";
 import { ActivityIndicator, StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { ThemeProvider } from "styled-components/native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <StatusBar
-          barStyle={"light-content"}
-          backgroundColor={"transparent"}
-          translucent
-        />
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor={"transparent"}
+        translucent
+      />
 
-        {fontsLoaded ? <Routes /> : <ActivityIndicator />}
-      </ThemeProvider>
+      {fontsLoaded ? <Routes /> : <ActivityIndicator />}
     </SafeAreaProvider>
   );
 }
