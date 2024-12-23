6d037890d04c86c7ca0f65a868882eecc7171c4c
Bruno Duarte <bruno.duarte314@gmail.com>
2024-12-22 21:43:14 -0300

    Delete header component

--- src/components/header/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Container, Title } from "./style";
-import Logo from "@assets/logo.svg";
-
-export const Header = () => (
-  <Container>
-    <Logo />
-    <Title>Treine sua mente e o seu corpo</Title>
-  </Container>
-);
