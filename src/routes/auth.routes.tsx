2d5bd0dbe6fa08afb02a7ce150c994318c10b60d
Bruno Duarte <bruno.duarte314@gmail.com>
2024-12-22 22:25:29 -0300

    Add app routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -2,7 +2,7 @@ import {
   createNativeStackNavigator,
   NativeStackNavigationProp,
 } from "@react-navigation/native-stack";
-import { SignIn } from "@screens/signIn";
+import { SignIn } from "@screens/SignIn";
 import { SignUp } from "@screens/SignUp";
 
 type AuthRoutes = {
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,16 @@ import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
 import { gluestackUIConfig } from "config/gluestack-ui.config";
 import { Box } from "@gluestack-ui/themed";
+import { AppRoutes } from "./app.routes";
 
 export const Routes = () => {
+  const logged = true;
   DefaultTheme.colors.background = gluestackUIConfig.tokens.colors.gray700;
 
   return (
     <Box flex={1} bg={"$gray700"}>
       <NavigationContainer theme={DefaultTheme}>
-        <AuthRoutes />
+        {logged ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
