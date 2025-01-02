d04d082ceca9ca4dcb1b8f9d7e7cbf5d82528ba7
Bruno Duarte <bruno.duarte314@gmail.com>
2025-01-02 18:37:17 -0300

    Add AuthContext

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,92 @@
+import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
+import { storageUserGet, storageUserSave } from "@storage/storageUser";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+type SignInData = {
+  email: UserDTO["email"];
+  password: string;
+};
+
+type SignUpData = SignInData & {
+  name: UserDTO["name"];
+};
+
+export type AuthContextDataProps = {
+  user?: UserDTO;
+  signIn: (signInData: SignInData) => Promise<void>;
+  signUp: (signUpData: SignUpData) => Promise<void>;
+  isLoadingUserStorageData: boolean;
+};
+
+type AuthContextProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps
+);
+
+export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+  const [user, setUser] = useState<UserDTO>();
+  const [isLoadingUserStorageData, setIsLoadingUserStorageData] =
+    useState(true);
+
+  const signIn = async (signInData: SignInData) => {
+    try {
+      const { data }: { data: { user?: UserDTO } } = await api.post(
+        "/sessions",
+        signInData
+      );
+      const dataUser = data.user;
+
+      if (dataUser) {
+        setUser(dataUser);
+        await storageUserSave(dataUser);
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const signUp = async (signUpData: SignUpData) => {
+    try {
+      const { data }: { data: { user?: UserDTO } } = await api.post(
+        "/users",
+        signUpData
+      );
+      const dataUser = data.user;
+
+      if (dataUser) {
+        setUser(dataUser);
+        await storageUserSave(dataUser);
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const loadUserData = async () => {
+    try {
+      const userLogged = await storageUserGet();
+
+      if (userLogged) {
+        setUser(userLogged);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    loadUserData();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{ user, signIn, signUp, isLoadingUserStorageData }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
