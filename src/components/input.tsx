c975f44ba8967f7e6fe94c4b1576f99bd2492449
Bruno Duarte <bruno.duarte314@gmail.com>
2025-01-02 01:12:26 -0300

    Add errorMessage at Input component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,30 +1,49 @@
-import { Input as GluestackInput, InputField } from "@gluestack-ui/themed";
+import {
+  FormControl,
+  FormControlError,
+  FormControlErrorText,
+  Input as GluestackInput,
+  InputField,
+} from "@gluestack-ui/themed";
 import { ComponentProps } from "react";
 
 type Props = ComponentProps<typeof InputField> & {
+  errorMessage?: string;
   isReadOnly?: boolean;
 };
 
-export const Input = ({ isReadOnly = false, ...rest }: Props) => (
-  <GluestackInput
-    h={"$14"}
-    borderWidth={"$1"}
-    borderColor={"$gray700"}
-    borderRadius={"$md"}
-    $focus={{
-      borderWidth: 1,
-      borderColor: "$green500",
-    }}
-    isReadOnly={isReadOnly}
-    opacity={isReadOnly ? 0.5 : 1}
-  >
-    <InputField
-      px="$4"
-      bg="$gray700"
-      color="$white"
-      fontFamily="$body"
-      placeholderTextColor={"$gray300"}
-      {...rest}
-    />
-  </GluestackInput>
-);
+export const Input = ({ errorMessage, isReadOnly = false, ...rest }: Props) => {
+  const isInvalid = !!errorMessage;
+
+  return (
+    <FormControl isInvalid={isInvalid} mb="$4" w="$full">
+      <GluestackInput
+        h={"$14"}
+        borderWidth={"$1"}
+        borderColor={"$gray700"}
+        borderRadius={"$md"}
+        $focus={{
+          borderWidth: 1,
+          borderColor: "$green500",
+        }}
+        isReadOnly={isReadOnly}
+        opacity={isReadOnly ? 0.5 : 1}
+      >
+        <InputField
+          px="$4"
+          bg="$gray700"
+          color="$white"
+          fontFamily="$body"
+          placeholderTextColor={"$gray300"}
+          {...rest}
+        />
+      </GluestackInput>
+
+      <FormControlError>
+        <FormControlErrorText color="$red500">
+          {errorMessage}
+        </FormControlErrorText>
+      </FormControlError>
+    </FormControl>
+  );
+};
