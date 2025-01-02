83b36034b468b86e1ca621067ca223898853bd71
Bruno Duarte <bruno.duarte314@gmail.com>
2025-01-02 00:13:53 -0300

    Add handleUserPhotoSelect at profile screen

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -2,10 +2,64 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
-import { Center, Heading, Text, VStack } from "@gluestack-ui/themed";
-import { ScrollView, TouchableOpacity } from "react-native";
+import { Center, Heading, Text, useToast, VStack } from "@gluestack-ui/themed";
+import { Alert, ScrollView, TouchableOpacity } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+import { useState } from "react";
+import { ToastMessage } from "@components/ToastMessage";
 
 export const Profile = () => {
+  const [userPhotoURI, setUserPhotoURI] = useState(
+    "https://github.com/brunoobd.png"
+  );
+  const toast = useToast();
+
+  const handleUserPhotoSelect = async () => {
+    try {
+      const photoSelectionResult = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4],
+        quality: 1,
+        allowsEditing: true,
+      });
+
+      if (!photoSelectionResult.canceled) {
+        const photoSelectedURI = photoSelectionResult.assets[0].uri;
+
+        if (photoSelectedURI) {
+          const photoSelectedInfo = await FileSystem.getInfoAsync(
+            photoSelectedURI
+          );
+
+          if (photoSelectedInfo.exists) {
+            const isPhotoSelectedSize5MBOrLess =
+              photoSelectedInfo.size / 1024 / 1024 <= 5;
+
+            if (isPhotoSelectedSize5MBOrLess) {
+              setUserPhotoURI(photoSelectedURI);
+            } else {
+              toast.show({
+                placement: "top",
+                render: ({ id }) => (
+                  <ToastMessage
+                    id={id}
+                    title="Imagem muito grande"
+                    description="Selecione uma de até 5MB."
+                    action="error"
+                    onClose={() => toast.close(id)}
+                  />
+                ),
+              });
+            }
+          }
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -13,12 +67,12 @@ export const Profile = () => {
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
         <Center mt="$6" px="$10">
           <UserPhoto
-            source={{ uri: "https://github.com/brunoobd.png" }}
+            source={{ uri: userPhotoURI }}
             size="xl"
             alt="Imagem do usuário"
           />
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="$green500"
               fontFamily="$heading"
