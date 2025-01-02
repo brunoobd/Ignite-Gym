2b2f64827c1be017218c2b24092c28abeadb67a2
Bruno Duarte <bruno.duarte314@gmail.com>
2025-01-02 18:38:14 -0300

    Add useAuth to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,19 @@ import { AuthRoutes } from "./auth.routes";
 import { gluestackUIConfig } from "config/gluestack-ui.config";
 import { Box } from "@gluestack-ui/themed";
 import { AppRoutes } from "./app.routes";
+import { useAuth } from "@hooks/useAuth";
+import { Loading } from "@components/Loading";
 
 export const Routes = () => {
-  const logged = false;
+  const { user, isLoadingUserStorageData } = useAuth();
   DefaultTheme.colors.background = gluestackUIConfig.tokens.colors.gray700;
+
+  if (isLoadingUserStorageData) return <Loading />;
 
   return (
     <Box flex={1} bg={"$gray700"}>
       <NavigationContainer theme={DefaultTheme}>
-        {logged ? <AppRoutes /> : <AuthRoutes />}
+        {user ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
