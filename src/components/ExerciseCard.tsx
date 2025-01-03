c7108119218c35de59342f3d57a9846e954a47a6
Bruno Duarte <bruno.duarte314@gmail.com>
2025-01-02 22:28:40 -0300

    Receive exercise at ExerciseCard

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -1,3 +1,4 @@
+import { ExerciseDTO } from "@dtos/ExerciseDTO";
 import {
   Text,
   Heading,
@@ -6,43 +7,50 @@ import {
   VStack,
   Icon,
 } from "@gluestack-ui/themed";
+import { api } from "@services/api";
 import { ChevronRight } from "lucide-react-native";
 import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 
-type Props = TouchableOpacityProps;
+type Props = TouchableOpacityProps & {
+  exercise: ExerciseDTO;
+};
 
-export const ExerciseCard = ({ ...props }: Props) => (
-  <TouchableOpacity {...props}>
-    <HStack
-      bg="$gray500"
-      alignItems="center"
-      p="$2"
-      pr="$4"
-      rounded="$md"
-      mb="$3"
-    >
-      <Image
-        source={{
-          uri: "https://static.wixstatic.com/media/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp/v1/fill/w_350,h_375,al_c/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp",
-        }}
-        alt="Imagem do exercício"
-        w="$16"
-        h="$16"
+export const ExerciseCard = ({ exercise, ...rest }: Props) => {
+  const { thumb, name, series, repetitions } = exercise;
+
+  return (
+    <TouchableOpacity {...rest}>
+      <HStack
+        bg="$gray500"
+        alignItems="center"
+        p="$2"
+        pr="$4"
         rounded="$md"
-        mr="$4"
-        resizeMode="cover"
-      />
+        mb="$3"
+      >
+        <Image
+          source={{
+            uri: `${api.defaults.baseURL}/exercise/thumb/${thumb}`,
+          }}
+          alt="Imagem do exercício"
+          w="$16"
+          h="$16"
+          rounded="$md"
+          mr="$4"
+          resizeMode="cover"
+        />
 
-      <VStack flex={1}>
-        <Heading fontSize="$lg" color="$white" fontFamily="$heading">
-          Puxada frontal
-        </Heading>
-        <Text fontSize="$sm" color="$gray200" mt="$1" numberOfLines={2}>
-          3 séries x 12 repetições
-        </Text>
-      </VStack>
+        <VStack flex={1}>
+          <Heading fontSize="$lg" color="$white" fontFamily="$heading">
+            {name}
+          </Heading>
+          <Text fontSize="$sm" color="$gray200" mt="$1" numberOfLines={2}>
+            {series} séries x {repetitions} repetições
+          </Text>
+        </VStack>
 
-      <Icon as={ChevronRight} color="$gray300" />
-    </HStack>
-  </TouchableOpacity>
-);
+        <Icon as={ChevronRight} color="$gray300" />
+      </HStack>
+    </TouchableOpacity>
+  );
+};
