34a42729d4e1841323987aec1766e2a7e47bc599
Bruno Duarte <bruno.duarte314@gmail.com>
2025-01-02 23:37:10 -0300

    Receive history at HistoryCard

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -1,34 +1,43 @@
+import { HistoryDTO } from "@dtos/HistoryDTO";
 import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
 
-export const HistoryCard = () => (
-  <HStack
-    w="$full"
-    px="$5"
-    py="$4"
-    mb="$3"
-    bg="$gray600"
-    rounded="$md"
-    alignItems="center"
-    justifyContent="space-between"
-  >
-    <VStack mr="$5" flex={1}>
-      <Heading
-        color="$white"
-        fontSize="$md"
-        fontFamily="$heading"
-        textTransform="capitalize"
-        numberOfLines={1}
-      >
-        Costas
-      </Heading>
+type Props = {
+  history: HistoryDTO;
+};
 
-      <Text color="$gray100" fontSize="$lg" numberOfLines={1}>
-        Puxada frontal
+export const HistoryCard = ({ history }: Props) => {
+  const { name, group, hour } = history;
+
+  return (
+    <HStack
+      w="$full"
+      px="$5"
+      py="$4"
+      mb="$3"
+      bg="$gray600"
+      rounded="$md"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack mr="$5" flex={1}>
+        <Heading
+          color="$white"
+          fontSize="$md"
+          fontFamily="$heading"
+          textTransform="capitalize"
+          numberOfLines={1}
+        >
+          {group}
+        </Heading>
+
+        <Text color="$gray100" fontSize="$lg" numberOfLines={1}>
+          {name}
+        </Text>
+      </VStack>
+
+      <Text color="$gray300" fontSize="$md">
+        {hour}
       </Text>
-    </VStack>
-
-    <Text color="$gray300" fontSize="$md">
-      08:56
-    </Text>
-  </HStack>
-);
+    </HStack>
+  );
+};
